d7ef0268f5f3d8f3bc4d0a9f1620234345336ec9
LLEPT <103839621+lauuu-pt@users.noreply.github.com>
2024-03-22 20:16:27 +0000

    cvbnm

---
+++ src/MySNS.java
@@ -0,0 +1,9 @@
+
+public class MySNS {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Unimplemented method 'main'");
+	}
+
+}
