02f52a702ed0d1ad2adaaf4db19beefdae922819
LLEPT <103839621+lauuu-pt@users.noreply.github.com>
2024-04-08 00:20:15 +0100

    meio mid

--- src/server/mySNSServer.java
+++ src/server/mySNSServer.java
@@ -1,202 +1,189 @@
 package server;
-
-import java.io.*;
-import java.net.*;
-import java.util.*;
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.EOFException;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class mySNSServer {
-	
-	/**
-     * Método principal para iniciar o servidor mySNS.
-     * @param args Argumentos da linha de comando (não utilizados).
-     */
-    public static void main(String[] args) {
-    	
-        System.out.println("Servidor aberto!");
-		var server = new mySNSServer();
-		server.startServer();
+
+    public static void main(String[] args) throws IOException {
+        System.out.println("Servidor: main");
+        var server = new mySNSServer();
+        server.startServer();
     }
-    
-    
-    /**
-     * Método para iniciar o servidor.
-     */
-    public void startServer() {
-    	
+
+    public void startServer() throws IOException {
         try (var sSoc = new ServerSocket(23456)) {
             while (true) {
                 try {
                     var inSoc = sSoc.accept();
                     var newServerThread = new ServerThread(inSoc);
                     newServerThread.start();
-                } catch (IOException e) { // Exceção para lidar com problemas de Input/Output (I/O).
+                } catch (IOException e) {
                     e.printStackTrace();
                 }
             }
-        } catch (IOException e) { // Exceção para lidar com problemas de Input/Output (I/O).
-            e.printStackTrace();
         }
     }
-    
-    
-    /**
-     * Classe interna que implementa uma thread do servidor para cada cliente.
-     */
+
     class ServerThread extends Thread {
-    
         private Socket socket;
-        
-        /**
-         * Construtor da classe ServerThread.
-         * @param inSoc Socket para a conexão do cliente.
-         */
+
         ServerThread(Socket inSoc) {
             socket = inSoc;
-            System.out.println("Thread do servidor para cada cliente.");
+            System.out.println("Thread do servidor para cada cliente");
         }
-        
-        
-        /**
-         * Método que executa a thread do servidor.
-         */
+
         public void run() {
-        	
             try (var outStream = new ObjectOutputStream(socket.getOutputStream());
                  var inStream = new ObjectInputStream(socket.getInputStream())) {
 
                 String user = null;
                 Boolean bool = null;
-                
                 try {
-                
                     user = (String) inStream.readObject();
                     bool = (Boolean) inStream.readObject();
                     
-                    System.out.println("Thread - depois de receber  o usuário");
-                    
-                } catch (ClassNotFoundException e1) { // Lida com exceção no caso da classe não existir.
+                    System.out.println("Thread: depois de receber  o usuário");
+                } catch (ClassNotFoundException e1) {
                     e1.printStackTrace();
                 }
-                outStream.writeObject(true);
+                outStream.writeObject(true); 
                 
                 if(!bool) {
-                	
-                    // Cria um diretorio baseado no username dado pela variavel "user".
-                    var userDirectory = new File("/home/aluno-di/eclipse-workspace/SEG/src/projeto/server", user);
-                    System.out.println("Diretorio do utilizador: " + userDirectory.getAbsolutePath());
-    
-                    if (!userDirectory.exists()) { // Se o diretorio não existir
-                        System.out.println("Diretorio do utilizador: " + userDirectory.getAbsolutePath());
-                        
-                        if (userDirectory.mkdirs()) { // Cria diretorio para o utilizador.
-                            System.out.println("Criado um diretorio para o utilizador " + user);
-                            
-                        } else {
-                            System.out.println("Não foi possivel criar o diretorio para o utilizador " + user);
-                        }
-                    }
-    
-                    boolean allFilesReceived = true; // Verifica se todos os ficheiros foram recebidos com sucesso.
-    
-                    // Recebe e guarda os ficheiros no diretorio do utilizador.
-                    try {
-                        while (true) {
-                            Long fileSize = (Long) inStream.readObject();
-                            
-                            if (fileSize == -1) { // Fim da transferência de ficheiros cliente-servidor.
-                                System.out.println("Cliente acabou de enviar os ficheiros.");
-                                break;
-                            }
-                            
-                            String filename = (String) inStream.readObject();
-                            
-                            var outputFile = new File(userDirectory, filename);
-                            try (var outFileStream = new FileOutputStream(outputFile);
-                                 var outFile = new BufferedOutputStream(outFileStream)) {
-                                byte[] buffer = new byte[1024];
-                                int bytesRead;
-                                long remainingBytes = fileSize;
-                                while (remainingBytes > 0 && (bytesRead = inStream.read(buffer, 0, (int) Math.min(buffer.length, remainingBytes))) != -1) {
-                                    outFile.write(buffer, 0, bytesRead);
-                                    remainingBytes -= bytesRead;
-                                }
-                            } catch (IOException e) { // Exceção para lidar com problemas de Input/Output (I/O).
-                                e.printStackTrace();                                 
-                                allFilesReceived = false; // Nem todos os ficheiros foram recebidos com sucesso.
-                            }
-    
-                            System.out.println("Fim do ficheiro " + filename);
-                        }
-    
-    
-                    } catch (EOFException e) {
-                    	
-                        // Cliente desconectou abruptamente
-                        System.err.println("Cliente desconectado antes de todos os ficheiros terem sido recebidos.");
-                        
-                        allFilesReceived = false; // Nem todos os ficheiros foram recebidos com sucesso.
-                    } catch (ClassNotFoundException e1) {
-                        e1.printStackTrace();
-                        allFilesReceived = false; // Nem todos os ficheiros foram recebidos com sucesso.
-                    }
-    
-                    // Manda reconhecimento para o cliente que todos os ficheiros foram recebidos.
-                    outStream.writeObject(allFilesReceived); 
-                    System.out.println("Servidor reconhece que todos os ficheiros foram recebidos? " + allFilesReceived);
-                    
+	                
+	                var userDirectory = new File("/home/aluno-di/eclipse-workspace/SEG/src/server", user);
+	                System.out.println("User directory path: " + userDirectory.getAbsolutePath());
+	
+	                if (!userDirectory.exists()) {
+	                	System.out.println("User directory path: " + userDirectory.getAbsolutePath());
+	
+	                	
+	                    if (userDirectory.mkdirs()) {
+	                        System.out.println("Created directory for user: " + user);
+	                    } else {
+	                        System.out.println("Failed to create directory for user: " + user);
+	                    }
+	                }
+	
+	                boolean allFilesReceived = true; 
+	
+	                
+	                try {
+	                	
+	                	while (true) {
+	                	    Long fileSize = (Long) inStream.readObject();
+	                	    if (fileSize == -1) {
+	                	        System.out.println("Client finished sending files.");
+	                	        break;
+	                	    }
+	                	    
+	                	    String filename = (String) inStream.readObject();
+	                	    
+	                	
+	                	    
+	                	    var outputFile = new File(userDirectory, filename);
+	                	    try (var outFileStream = new FileOutputStream(outputFile);
+	                	         var outFile = new BufferedOutputStream(outFileStream)) {
+	                	        byte[] buffer = new byte[1024];
+	                	        int bytesRead;
+	                	        long remainingBytes = fileSize;
+	                	        while (remainingBytes > 0 && (bytesRead = inStream.read(buffer, 0, (int) Math.min(buffer.length, remainingBytes))) != -1) {
+	                	            outFile.write(buffer, 0, bytesRead);
+	                	            remainingBytes -= bytesRead;
+	                	        }
+	                	    } catch (IOException e) {
+	                	        e.printStackTrace();
+	                	        allFilesReceived = false;
+	                	    }
+	
+	                	    System.out.println("End of file: " + filename);
+	                	}
+	
+	
+	                } catch (EOFException e) {
+	                
+	                    System.err.println("Client disconnected before all files were received.");
+	                    allFilesReceived = false; 
+	                } catch (ClassNotFoundException e1) {
+	                    e1.printStackTrace();
+	                    allFilesReceived = false; 
+	                }
+	
+	                
+	                outStream.writeObject(allFilesReceived); 
+	                System.out.println("Server acknowledges successful file transfer: " + allFilesReceived);
                 } else {
-                    int lenFicheiros = (int)inStream.readObject();
-                    for(int i = 0; i < lenFicheiros; i++){
-                        List<File> FilesServer = new ArrayList<File>();
-                        String nomeFicheiro = (String) inStream.readObject();
-                        var Diretorio  = new File("/home/aluno-di/eclipse-workspace/SEG/src/projeto/server", user);
-                        File[] files = Diretorio.listFiles();
-                        
-                        // Itera sobre os arquivos e verifica se começam com o nome do ficheiro
-                        if (files != null) {
-                            for (File file : files) {
-                                if (file.isFile() && file.getName().startsWith(nomeFicheiro)){
-                                    FilesServer.add(file);
-                                }
-                            }
-                            
-                            outStream.writeObject(FilesServer.size());
-                            
-                            for(int j =0; j<FilesServer.size(); j++) {
-                                outStream.writeObject(FilesServer.get(j).getName());
-                                outStream.writeObject(FilesServer.get(j).length());
-                                
-                                try (BufferedInputStream cifradoFileB = new BufferedInputStream(new FileInputStream(FilesServer.get(j)))) {
-                                    byte[] buffer = new byte[1024];
-                                    int bytesRead;
-                                    while ((bytesRead = cifradoFileB.read(buffer, 0, 1024)) > 0) {
-                                        outStream.write(buffer, 0, bytesRead);
-                                    }
-                                }
-                            }
-                        } else {
-                            System.out.println("O caminho especificado não é um diretório.");
-                        }
-                    }
-                                      
+                	try {
+                		var userDirectory = new File("/home/aluno-di/eclipse-workspace/SEG/src/server", user);
+                		String filename = (String) inStream.readObject();
+                		File file = new File(filename);
+                		System.out.println("AAAAAAAAAAAAAAAAA");
+                			String prefix = filename;
+                			List<File> filesFound = new ArrayList<>();
+                			File[] files = userDirectory.listFiles();
+                			System.out.println("BBBBBBBBBBBB");
+                			for (File f : files) {
+                		        
+                		        if (f.getName().startsWith(prefix)) {
+                		            
+                		            filesFound.add(f);
+                		            System.out.println("CCCCCCCCCCCCC");
+                		        }
+                		    }
+                			for (File fi: filesFound) {
+                			    
+                			    outStream.writeObject(fi.getName());
+                			    System.out.println(fi.getName());
+                			    
+                			    outStream.writeObject(fi.length());
+                			    System.out.println(fi.length());
+                			    
+                			    try (BufferedInputStream myFileB = new BufferedInputStream(new FileInputStream(fi))) {
+                			        byte[] buffer = new byte[1024];
+                			        int bytesRead;
+                			        while ((bytesRead = myFileB.read(buffer)) != -1) {
+                			            outStream.write(buffer, 0, bytesRead);
+                			            System.out.println("FFFFFFFFFFFF");
+                			        }
+                			    }
+                			}
+
+                			outStream.close();
+                			inStream.close();
+            		    
+                	} catch(Exception e ){
+                		
+                	}
                 }
-            } catch (IOException e) {
-                System.err.println("Erro na comunicação com o cliente: " + e.getMessage());
-                if (e instanceof EOFException) {
-                    System.err.println("O cliente encerrou abruptamente a conexão.");
-                } else if (e instanceof SocketException) {
-                    System.err.println("Erro de socket: " + e.getMessage());
-                }
-            } catch (ClassNotFoundException e) {
-				e.printStackTrace();
-			} finally {
-                try {
-                    socket.close();
-                    System.out.println("Conexão com o cliente encerrada.");
-                } catch (IOException e) {
-                    System.err.println("Erro ao fechar o socket: " + e.getMessage());
-                }
-            }
-        }
-    }	
-}
+	            } catch (IOException e) {
+	                System.err.println("Erro na comunicação com o cliente: " + e.getMessage());
+	                if (e instanceof EOFException) {
+	                    System.err.println("O cliente encerrou abruptamente a conexão.");
+	                } else if (e instanceof SocketException) {
+	                    System.err.println("Erro de soquete: " + e.getMessage());
+	                }
+	            } finally {
+	                try {
+	                    socket.close();
+	                    System.out.println("Conexão com o cliente encerrada.");
+	                } catch (IOException e) {
+	                    System.err.println("Erro ao fechar o socket: " + e.getMessage());
+	                }
+	            }
+            
+        
+    }
+    }}
